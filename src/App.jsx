b33c746c9edf02ebf735e5da1eacf6c5a15de58a
Jack Winkler <jack.p.winkler@gmail.com>
2025-06-23 19:42:38 -0400

    layout setup with pages and css adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Navbar from "./components/Navbar.jsx"
 import SycamorePost from './pages/About/SycamorePost'
 import Tiffany from './pages/About/Tiffany'
 import Jarrah from './pages/About/Jarrah'
 import Book1 from './pages/Books/Book1'
 import Book2 from './pages/Books/Book2'
+import Books from './pages/Books/Books'
+import Events from './pages/Events/Events'
+import Resources from './pages/Resources/Resources'
+import stackOfBooks from './assets/stackofbooks.webp';
 
 function App() {
   return (
@@ -17,16 +22,42 @@ function App() {
         <Route path="/about/jarrah" element={<Jarrah />} />
         <Route path="/books/book1" element={<Book1 />} />
         <Route path="/books/book2" element={<Book2 />} />
+        <Route path="/books/all" element={<Books />} />
+        <Route path="/events/all" element={<Events />} />
+        <Route path="/resources/all" element={<Resources />} />
         
         <Route
           path="/"
           element={
             <>
               <h1>Sycamore Post</h1>
-              <h2 id = "About">About</h2>
-              <h2 id = "Books">Books</h2>
-              <h2 id = "Events">Events</h2>
-              <h2 id = "Resources">Resources</h2>
+              <section id="About">
+                <Link to="/about/sycamore-post" className="header-buttons">
+                  About
+                </Link>
+              </section>
+              <p>
+                The sycamore post is a bookstore that makes books about the environment
+              </p>
+              <p>Tiffany Winkler is the author</p>
+              <p>more stuff about it</p>
+              <p></p>
+              <section id="Books">
+                <Link to="/books/all" className="header-buttons">
+                  Books
+                </Link>
+                <img src={stackOfBooks} alt="books image" className="books-image"></img>
+              </section>              
+              <section id="Events">
+                <Link to="/events/all" className="header-buttons">
+                  Events
+                </Link>
+              </section>
+              <section id="Resources">
+                <Link to="/resources/all" className="header-buttons">
+                  Resources
+                </Link>
+              </section>            
             </>
           }
         />
